7eed0281852dbdc266770983fe4cc34523968c95
Layne Haber <layne.haber@gmail.com>
2025-06-03 15:19:15 -0600

    feat: enable container insights

--- ops/modules/ecs/main.tf
+++ ops/modules/ecs/main.tf
@@ -1,5 +1,11 @@
 resource "aws_ecs_cluster" "infrastructure" {
   name = "${var.ecs_cluster_name_prefix}-${var.domain}-${var.environment}-${var.stage}"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
   tags = {
     Stage       = var.stage
     Environment = var.environment
